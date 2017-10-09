b5c160e0e5e80d414cb396268b91d7fef8c6df89
Paul Arizpe <paul@iterando.mx>
2017-10-08 22:28:10 -0500

    Lab 1: Ejercicio 2

--- csharpavanzado/csharpavanzado.Lab01/MainWindow.xaml.cs
+++ csharpavanzado/csharpavanzado.Lab01/MainWindow.xaml.cs
@@ -24,7 +24,8 @@ namespace csharpavanzado.Lab01
         public MainWindow()
         {
             InitializeComponent();
-            CreateTask();
+            // CreateTask();
+            RunTaskGroup();
         }
 
         void CreateTask()
@@ -64,6 +65,29 @@ namespace csharpavanzado.Lab01
 
             // Otra alternativa es usar el metodo estatico Task.Run como atajo al metodo Task.Factory.StartNew
             var T9 = Task.Run(() => AddMessage("Tarea ejecutada con Task.Run"));
+
+            // Task Await
+            var T10 = Task.Run(() =>
+            {
+                WriteToOutput("Iniciando tarea 10...");
+                // AddMessage("Iniciando tarea 10...");
+                // Simular un proceso que dura 10 segundos
+                Thread.Sleep(10000); // El hilo es suspendido por 10000 milsegundos
+                WriteToOutput("Fin de la tarea 10.");
+                // AddMessage("Fin de la tarea 10.");
+            });
+            WriteToOutput("Esperando a la tarea 10.");
+            // AddMessage("Esperando a la tarea 10.");
+            T10.Wait();
+            WriteToOutput("La tarea 10 finalizo su ejecucion.");
+            // AddMessage("La tarea 10 finalizo su ejecucion.");
+            // hay un extra que dice que cambiemos WriteToOuput x AddMessage
+            // El resultado fue que no cargo la aplicacion jajaja, imagino que nunca terminan los hilos y pasa eso
+            // debido a que ejecutamos otro hilo con el Dispatcher Invoke XD
+
+            // Task.WaitAll
+            // Se utiliza cuando esperamos que multiples tareas finalicen su ejecucion, se deben agregar las tareas en un arreglo
+
         }
 
         void ShowMessage()
@@ -92,6 +116,39 @@ namespace csharpavanzado.Lab01
             System.Diagnostics.Debug.WriteLine(
                 $"Mensaje: {message}, " +
                 $"Hilo actual: {Thread.CurrentThread.ManagedThreadId}");
+        }
+        /// <summary>
+        /// Muestra el momento en que inicia y el momento en que finaliza la ejecucion de un proceso
+        /// que dura 10 segundos
+        /// </summary>
+        /// <param name="taskNumber"></param>
+        void RunTask(byte taskNumber)
+        {
+            WriteToOutput($"Iniciando tarea {taskNumber}");
+            // Similar un proceso que dura 10 segundos
+            Thread.Sleep(10000); // El hilo es supendido por 10000 milisegundos
+            WriteToOutput($"Finalizando tarea {taskNumber}");
+        }
+
+        void RunTaskGroup()
+        {
+            Task[] TaskGroup = new Task[]
+            {
+                Task.Run(() => RunTask(1)),
+                Task.Run(() => RunTask(2)),
+                Task.Run(() => RunTask(3)),
+                Task.Run(() => RunTask(4)),
+                Task.Run(() => RunTask(5))
+            };
+            // Task.WaitAll
+            //WriteToOutput($"Esperando a que finalicen todas las tareas...");
+            //Task.WaitAll(TaskGroup);
+            //WriteToOutput($"Todas las tareas han finalizado.");
+
+            // Task.WaitAny
+            WriteToOutput("Esperando a que finalice al menos una tarea");
+            Task.WaitAny(TaskGroup);
+            WriteToOutput("Al menos una tarea finalizo.");
         }
     }
 }
